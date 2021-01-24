f66cdec4841dbdfaa3089ad9f092775a0edb18d3
dillonharlessNHRMC <44904226+dillonharlessNHRMC@users.noreply.github.com>
2021-01-23 21:59:14 -0500

    Set up

---
+++ 01BubbleSort.py
@@ -0,0 +1,38 @@
+"""
+Read in a file (called "numbers.txt") of integers.  There will be one integer per line.
+Your program should:
+1) read the file and keep all the values in a list of integers
+2) implement a bubble sort algorithm in a function called bubble_sort(),
+      use the pseudocode is provided in the textbook, in question 2-2.
+3) use the bubble_sort() function to sort the values in the list
+4) write the sorted list into an output file called out.txt
+
+"""
+
+def bubble_sort(A):
+# implement your bubble sort here
+
+
+
+
+# open the input file
+input_numbers = open("numbers.txt", "r")
+
+# create an empty list
+nums = []
+# read in the lines from the input file, and add the values to the list
+for line in input_numbers:
+
+
+
+
+# bubble sort
+
+
+
+# write the sorted values into the output file, one value each line.
+
+
+
+
+
